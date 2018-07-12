0117cd1b0f7c0d1e677885940f5808de2bffa382
nepeat <nepeat@users.noreply.github.com>
2018-07-12 05:44:04 -0700

    Separate push to function, add statsd tags, push per cell.

--- statsd-power/statsd.lua
+++ statsd-power/statsd.lua
@@ -20,14 +20,26 @@ local push_timer = event.timer(1, function()
     event.push("push")
 end, math.huge)
 
+local function push()
+    -- Draconic cap data
+    if component.isAvailable("draconic_rf_storage") then
+        for cell_address, _ in pairs(component.list("draconic_rf_storage")) do
+            local cell = component.proxy(cell_address)
+            local energy = cell.getEnergyStored()
+            local tag = "rf_storage,type=draconic,computer=" .. computerid .. ",cell=" .. cell_address
+
+            gauge(tag, energy)
+            print(cell_address .. " - " .. tostring(energy) .. " RF.")
+        end
+    end
+end
+
 local function main()
     while true do
         local ev = {event.pull()}
 
         if ev[1] == "push" then
-            local energy = component.draconic_rf_storage.getEnergyStored()
-            gauge("rf_storage." .. computerid, energy)
-            print(tostring(energy) .. " RF.")
+            push()
         end
         -- Control events
         if ev[1] == "interrupted" then
